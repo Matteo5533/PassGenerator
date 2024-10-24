4944d9aa68b66771f44a8fa85574ccef2f6190a9
Matteo <matteondrekaj9@gmail.com>
2024-10-24 16:32:24 +0200

    PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -5,7 +5,7 @@ characters = ''
 
 lenght = int(input("Enter password length "))
 upper = int(bool(input("Enter 1 if capital letters are desired or 0 if not desired ")))
-if upper:
+if upper >= 1:
     characters += string.ascii_uppercase
 lower = int(bool(input("Enter 1 if lower case is desired or 0 if not desired ")))
 if lower:
